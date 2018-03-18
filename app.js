1b23341402bc3eeefb6a97ec302c95914f9df9f6
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-18 19:37:32 +0000

    idk

--- app.js
+++ app.js
@@ -133,4 +133,12 @@ app.get("/loadallusers/", function(req, res) {
 
 });
 
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static('client/build'));
+   const path = require('path');
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   })
+}
+
 app.listen(process.env.PORT)
