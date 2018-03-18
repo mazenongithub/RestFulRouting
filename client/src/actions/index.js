fd5c540fe5e475f959352efc544fc7affdd7b186
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-18 20:21:58 +0000

    idk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,14 +2,8 @@ import axios from 'axios';
 import { LOAD_USER } from './loaduser';
 axios.defaults.withCredentials = true;
 export const loadUser = () => async dispatch => {
-    var baseurl = ""
-    if (process.env.Node_ENV === 'production') {
-        baseurl = ""
-    }
-    else {
-        baseurl = "https://webdevbootcamp-mazenoncloud9.c9users.io:8080";
-    }
 
-    const res = await axios.get(baseurl + "/loadallusers");
+
+    const res = await axios.get("/loadallusers");
     dispatch({ type: LOAD_USER, payload: res.data });
 };
