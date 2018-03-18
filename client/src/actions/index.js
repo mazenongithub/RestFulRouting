a71303f9f61c7dcbc6e73a0e44a0a7dd0a0fe912
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-18 20:48:02 +0000

    Finally got React-Router working

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,8 +2,6 @@ import axios from 'axios';
 import { LOAD_USER } from './loaduser';
 axios.defaults.withCredentials = true;
 export const loadUser = () => async dispatch => {
-    const theurl = null;
-
     //"https://webdevbootcamp-mazenoncloud9.c9users.io:8081/loadallusers"
 
     const res = await axios.get("/loadallusers");
