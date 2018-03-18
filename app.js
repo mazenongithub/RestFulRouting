993000466c4c55fccf0ae5745a2beae41a6cd654
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-18 19:16:08 +0000

    idk

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var app = express();
 var bodyParser = require("body-parser");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
+// JavaScript Document
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 //7 Restful Routes are: 1) index /app-name get, list all values 2) new /app-name/new get show new form,  3) create /app-name post create item then redirect
 //4) show  /app-name/:id get show info about one item // 5) edit app-name/:id/edit show edit form for one value get 
 //6)  update app-name/:id put update value then redirect 7) delete app-name/:id destroy delete then redirect
@@ -22,6 +26,9 @@ app.get("/", function(req, res) {
          console.log("you have an error");
       }
       else {
+
+
+
          res.render("indextemplate", { people: succ, mode: 2 })
       }
 
@@ -125,7 +132,5 @@ app.get("/loadallusers/", function(req, res) {
    }); // end of find   
 
 });
-
-
 
 app.listen(process.env.PORT)
