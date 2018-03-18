5e8ffdc76cb4180bc6c485217f3a0bf68e87ac92
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-18 20:24:59 +0000

    idk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,7 +3,7 @@ import { LOAD_USER } from './loaduser';
 axios.defaults.withCredentials = true;
 export const loadUser = () => async dispatch => {
 
-
+    console.log(process.env.NODE_ENV);
     const res = await axios.get("/loadallusers");
     dispatch({ type: LOAD_USER, payload: res.data });
 };
