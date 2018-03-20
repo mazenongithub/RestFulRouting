35ab1c78fbc506f28c46feb27afe1baad2935d25
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-20 18:04:29 +0000

    Uploading to heroku

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,16 +3,16 @@ import { LOAD_USER } from './loaduser';
 import { CREATE_POST } from './createpost';
 
 axios.defaults.withCredentials = true;
-
+//https://webdevbootcamp-mazenoncloud9.c9users.io:8080
 export const loadUser = () => async dispatch => {
-    //loadallusers
-    const res = await axios.get("https://webdevbootcamp-mazenoncloud9.c9users.io:8080/loadallusers");
+
+    const res = await axios.get("/loadallusers");
     dispatch({ type: LOAD_USER, payload: res.data });
 };
 
 export const createPost = (values, callback) => async dispatch => {
-    //loadallusers
-    const res = await axios.post("https://webdevbootcamp-mazenoncloud9.c9users.io:8080/RestFulRouting", values);
+
+    const res = await axios.post("/RestFulRouting", values);
     callback();
     dispatch(
 
