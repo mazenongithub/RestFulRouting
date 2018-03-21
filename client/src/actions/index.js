4dbb912c02885514b32571391427e7807f58c3fd
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-21 22:35:56 +0000

    Got show route working

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 export const CREATE_POST = 'create_post';
 export const LOAD_USER = 'load_user';
 export const SHOW_USER = 'show_user';
+export const FETCH_USER = 'fetch_user';
 axios.defaults.withCredentials = true;
 //https://webdevbootcamp-mazenoncloud9.c9users.io:8080
 export const loadUser = () => async dispatch => {
@@ -17,4 +18,13 @@ export const createPost = (values, callback) => async dispatch => {
     dispatch(
 
         { type: CREATE_POST, payload: res.data });
+};
+
+export const fetchUser = (id) => async dispatch => {
+
+    const res = await axios.get(`https://webdevbootcamp-mazenoncloud9.c9users.io:8080/restfulrouting/${id}/show`);
+
+    dispatch(
+
+        { type: FETCH_USER, payload: res.data });
 };
--- client/src/components/showuser.js
+++ client/src/components/showuser.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
 
 class ShowUser extends Component {
 
+    componentDidMount() {
+        var user_id = this.props.match.params.id;
+
+        this.props.fetchUser(user_id);
+
+    }
+
     render() {
+        for (var i = 0; i < this.props.myuser.length; i++) {
+            var firstname = this.props.myuser[i].firstname
+            var lastname = this.props.myuser[i].lastname;
+        }
 
         return (
 
             <table width="95%" border="0" cellspacing="0" cellpadding="10">
   <tr>
     <th width="24%" scope="col">First Name</th>
-    <td width="76%">&nbsp;</td>
+    <td width="76%">{firstname}</td>
   </tr>
   <tr>
     <th>Last Name</th>
-    <td>&nbsp;</td>
+    <td>{lastname}</td>
   </tr>
 </table>
 
@@ -31,4 +43,8 @@ class ShowUser extends Component {
 
 } //End Component
 
-export default ShowUser;
+function mapStateToProps(state) {
+    return { myuser: state.user };
+}
+
+export default connect(mapStateToProps, { fetchUser })(ShowUser);
