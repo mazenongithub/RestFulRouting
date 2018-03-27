b7a66b26cb5ce76eeeea2d3f61d43c9b5a186c12
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-27 22:53:10 +0000

    updated index page before adding form

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-import Landing from './landing';
-import PostsNew from './form'
 import ShowUser from './showuser';
 import Master from './master'
 
@@ -16,9 +14,7 @@ class App extends Component {
     <div>
     <Switch>
     <Route exact path="/reactrouter/:id/show" component={ShowUser} />
-    <Route exact path="/reactrouter/restfulrouting" component={PostsNew} />
-    <Route exact path="/reactrouter/allusers" component={Master} />
-    <Route exact path="/" component={Landing} />
+     <Route exact path="/" component={Master} />
     </Switch>
     </div>
     </BrowserRouter>
