d0fc33e196ac21d4b103e36fbbbdad2cd3cdb79b
MazenonCloud9 <mazen@egeotechnical.com>
2018-03-29 22:28:28 +0000

    deleted files

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
-import ShowUser from './showuser';
 import Master from './master'
 
 
@@ -13,8 +12,7 @@ class App extends Component {
       <BrowserRouter>
     <div>
     <Switch>
-    <Route exact path="/reactrouter/:id/show" component={ShowUser} />
-     <Route exact path="/" component={Master} />
+    <Route exact path="/" component={Master} />
     </Switch>
     </div>
     </BrowserRouter>
